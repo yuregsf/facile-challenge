d7916777ed61a74ebd9d1405fbffeed243751a31
yuregsf <yuregsfpb@gmail.com>
2021-12-02 19:11:31 -0300

    ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,19 +1,19 @@
 module.exports = {
-  connection: process.env.CONNECTION,
+  type: process.env.CONNECTION,
   host: process.env.HOST,
+  port: process.env.PORT,
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.PORT,
   logging: process.env.LOGGING,
-  entities: process.env.ENTITIES,
-  migrations: process.env.MIGRATIONS,
+  entities: [process.env.ENTITIES],
+  migrations: [process.env.MIGRATIONS],
   migrations_run: process.env.MIGRATIONS_RUN,
   ssl:
-      process.env.SSL === 'true'
-        ? {
-            rejectUnauthorized: false,
-          }
-        : false,
+    process.env.SSL === 'true'
+      ? {
+        rejectUnauthorized: false,
+      }
+      : false,
 
 }
